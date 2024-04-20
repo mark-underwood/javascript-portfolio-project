5852198a659a32108a03761dc9053b1354272190
Mark Underwood <mark.thunderwood@gmail.com>
2024-04-20 15:14:20 -0500

    attempted ui fix, dummy button events

---
+++ js/js-portfolio-pj.js
@@ -0,0 +1,43 @@
+// Reference: https://www.sololearn.com/en/compiler-playground/WUVrS54WbnSg/
+
+// Start (Left side controller)
+// Left
+function sButtonLeft() {
+    console.log(`s-L: Thrust left activated.`);
+    // alert(`s-L: Thrust left off.`);
+}
+function sButtonLeftOff() {
+    console.log(`s-L: Thrust left off.`);
+    // alert(`s-L: Thrust left off.`);
+}
+
+// Up
+function sButtonUp() {
+    console.log(`s-U: Thrust up activated.`);
+    // alert(`s-U: Thrust up activated.`);
+}
+function sButtonUpOff() {
+    console.log(`s-U: Thrust up off.`);
+    // alert(`s-U: Thrust up off.`);
+}
+
+// End (Right side controller)
+// Up
+function eButtonUp() {
+    console.log(`e-U: Thrust up activated.`);
+    // alert(`e-U: Thrust up activated.`);
+}
+function eButtonUpOff() {
+    console.log(`e-U: Thrust up off.`);
+    // alert(`e-U: Thrust up off.`);
+}
+
+// Right
+function eButtonRight() {
+    console.log(`e-R: Thrust right activated.`);
+    // alert(`e-R: Thrust right activated.`);
+}
+function eButtonRightOff() {
+    console.log(`e-R: Thrust right off.`);
+    // alert(`e-R: Thrust right off.`);
+}
