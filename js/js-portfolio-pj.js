87c35de5c63dc0a69aee236597713bed1fd1de55
Mark Underwood <mark.thunderwood@gmail.com>
2024-04-21 01:11:06 -0500

    long-press context menu prevention for mobile

--- js/js-portfolio-pj.js
+++ js/js-portfolio-pj.js
@@ -1,26 +1,35 @@
 // Reference: https://www.sololearn.com/en/compiler-playground/WUVrS54WbnSg/
 
+window.oncontextmenu = function(event) {
+    // solution to prevent long press menu on touch devices
+    // by user bbsimonbb on stackoverflow
+    // https://stackoverflow.com/questions/3413683/disabling-the-context-menu-on-long-taps-on-android
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+}
+
 // Start (Left side controller)
 // Left
 function sButtonLeft(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`s-L: Thrust left activated.`);
     // alert(`s-L: Thrust left activated.`);
 }
 function sButtonLeftOff(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`s-L: Thrust left off.`);
     // alert(`s-L: Thrust left off.`);
 }
 
 // Up
 function sButtonUp(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`s-U: Thrust up activated.`);
     // alert(`s-U: Thrust up activated.`);
 }
 function sButtonUpOff(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`s-U: Thrust up off.`);
     // alert(`s-U: Thrust up off.`);
 }
@@ -28,24 +37,24 @@ function sButtonUpOff(event) {
 // End (Right side controller)
 // Up
 function eButtonUp(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`e-U: Thrust up activated.`);
     // alert(`e-U: Thrust up activated.`);
 }
 function eButtonUpOff(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`e-U: Thrust up off.`);
     // alert(`e-U: Thrust up off.`);
 }
 
 // Right
 function eButtonRight(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`e-R: Thrust right activated.`);
     // alert(`e-R: Thrust right activated.`);
 }
 function eButtonRightOff(event) {
-    event.stopPropagation();
+    // event.stopPropagation();
     console.log(`e-R: Thrust right off.`);
     // alert(`e-R: Thrust right off.`);
 }
