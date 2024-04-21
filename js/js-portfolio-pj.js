2a6ef35e691876f075dbc89a2429ede52ad7fcf9
Mark Underwood <mark.thunderwood@gmail.com>
2024-04-20 21:54:01 -0500

    js event and stopPropagation()

--- js/js-portfolio-pj.js
+++ js/js-portfolio-pj.js
@@ -2,42 +2,50 @@
 
 // Start (Left side controller)
 // Left
-function sButtonLeft() {
+function sButtonLeft(event) {
+    event.stopPropagation();
     console.log(`s-L: Thrust left activated.`);
     // alert(`s-L: Thrust left activated.`);
 }
-function sButtonLeftOff() {
+function sButtonLeftOff(event) {
+    event.stopPropagation();
     console.log(`s-L: Thrust left off.`);
     // alert(`s-L: Thrust left off.`);
 }
 
 // Up
-function sButtonUp() {
+function sButtonUp(event) {
+    event.stopPropagation();
     console.log(`s-U: Thrust up activated.`);
     // alert(`s-U: Thrust up activated.`);
 }
-function sButtonUpOff() {
+function sButtonUpOff(event) {
+    event.stopPropagation();
     console.log(`s-U: Thrust up off.`);
     // alert(`s-U: Thrust up off.`);
 }
 
 // End (Right side controller)
 // Up
-function eButtonUp() {
+function eButtonUp(event) {
+    event.stopPropagation();
     console.log(`e-U: Thrust up activated.`);
     // alert(`e-U: Thrust up activated.`);
 }
-function eButtonUpOff() {
+function eButtonUpOff(event) {
+    event.stopPropagation();
     console.log(`e-U: Thrust up off.`);
     // alert(`e-U: Thrust up off.`);
 }
 
 // Right
-function eButtonRight() {
+function eButtonRight(event) {
+    event.stopPropagation();
     console.log(`e-R: Thrust right activated.`);
     // alert(`e-R: Thrust right activated.`);
 }
-function eButtonRightOff() {
+function eButtonRightOff(event) {
+    event.stopPropagation();
     console.log(`e-R: Thrust right off.`);
     // alert(`e-R: Thrust right off.`);
 }
